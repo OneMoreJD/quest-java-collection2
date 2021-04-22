7fb12dae41c881e6e10aa0fd3f839aac5f1d5859
duvaudierje <jerome.duvaudier@natixis.com>
2021-04-22 16:14:04 +0200

    resolution quete collection 02

--- Thanos.java
+++ Thanos.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Thanos {
@@ -10,16 +12,25 @@ public class Thanos {
         heroes.add(new Hero("Black Widow", 34));
         heroes.add(new Hero("Captain America", 100));
         heroes.add(new Hero("Thor", 1501));
-
+        
+        
         // TODO 1 : Modify Hero to implements Comparable and sort by name (ascending)
-
-
+        Collections.sort(heroes);
+        
         System.out.println("\nOrder by name:");
         showList(heroes);
-
+        
+        
         // TODO 2: Add a Comparator and sort by age (descending)
+        Comparator<Hero> heroComparator = new Comparator<Hero>() {
 
+			@Override
+			public int compare(Hero h1, Hero h2) {
+				return h2.getAge() - h1.getAge();
+			}
+		};
 
+		Collections.sort(heroes, heroComparator);
         System.out.println("\nOrder by age:");
         showList(heroes);
     }
